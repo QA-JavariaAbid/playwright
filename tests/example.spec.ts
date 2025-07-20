516b0f3490ac1166241059d2af0dd6148a78f194
QA-JavariaAbid <javariaabid079@gmail.com>
2025-07-20 17:23:32 +0500

    2

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,7 +4,7 @@ test('has title', async ({ page }) => {
   await page.goto('https://www.google.com/');
   await expect(page).toHaveTitle('assadadadad')
 
-  console.log('thid check')
+  console.log('a check')
 
  
 
